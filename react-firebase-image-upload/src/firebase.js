daa0b500c620e8db4efc840a08b29f444e89d541
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-08-10 22:32:51 +0530

    Success Image Upload to Firebase

---
+++ react-firebase-image-upload/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBQRTVO24XU2qBJEK3ZAeCqANjlj7PKB74",
+    authDomain: "fir-react-image-6be19.firebaseapp.com",
+    projectId: "fir-react-image-6be19",
+    storageBucket: "fir-react-image-6be19.appspot.com",
+    messagingSenderId: "660112855967",
+    appId: "1:660112855967:web:19a55da35066be58b863aa"
+}
+
+firebase.initializeApp(config);
+
+export default firebase
